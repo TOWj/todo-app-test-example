db3bc176b7816bc5df02277218ccae9da1e55979
Yuriy Novikov <tow1987@yandex.ru>
2022-12-02 17:20:05 +0300

    Initial commit

---
+++ src/main/java/com/example/todoapptestexample/TodoAppTestExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.todoapptestexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppTestExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoAppTestExampleApplication.class, args);
+    }
+
+}
