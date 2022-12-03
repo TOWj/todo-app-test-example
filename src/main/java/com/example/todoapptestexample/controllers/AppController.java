08db227f4f25f853ba2cafcd719fd1fb3b026abb
Yuriy Novikov <tow1987@yandex.ru>
2022-12-03 19:51:35 +0300

    Верстка разбита и почти готова

--- src/main/java/com/example/todoapptestexample/controllers/AppController.java
+++ src/main/java/com/example/todoapptestexample/controllers/AppController.java
@@ -11,4 +11,19 @@ public class AppController {
         return "index";
     }
 
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/todo")
+    public String todoPage() {
+        return "todo-page";
+    }
+
 }
